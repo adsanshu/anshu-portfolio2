c1cbd9ebee5a8917bca87bf416edfcbe0e472cec
adsanshu <adsanshu123@gmail.com>
2026-09-13 16:42:23 +0530

    Import ExplorePage component in main.jsx

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,1 +1,16 @@
+import React from "react";
 
+function ExplorePage({ onBack }) {
+  return (
+    <div className="explorePage">
+      <button onClick={onBack}>
+        ← Back
+      </button>
+
+      <h1>Explore</h1>
+      <p>Welcome to Anshu's World.</p>
+    </div>
+  );
+}
+
+export default ExplorePage;
